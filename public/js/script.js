20b12060fab8034dfbe48a66f51168732442ed41
Sean Baklini <sbaklini@unm.edu>
2020-05-12 22:16:02 -0600

    Get request set up

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    let posts;
+    function getPosts() {
+        $.get("/api/posts", function (data) {
+            console.log("Posts: " + data);
+            posts = data;
+            setPosts();
+        })
+    }
 
-function getPosts() {
-    $.get("/api/posts", function (data) {
-        console.log("Posts: " + data);
-        setPosts();
-    })
-}
+    function setPosts() {
 
-function setPosts() {
-    
-}
-    
-    $('button').on("submit", function(event) {
-    
-    }).
+
+    }
+
+
+    function postnewBlog() {
+        $('button').on("submit", function (event) {
+
+        })
+
+    }
+
+    function deleteBlog() {
+        $('button').on("click", function (event) {
+
+        })
+    }
+
 
 });
