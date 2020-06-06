480408d75223b03dc9f84a8f7a011ea950c13a3a
Sean Baklini <sbaklini@unm.edu>
2020-06-06 15:41:22 -0600

    Changed format of application

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,22 +2,23 @@
 var path = require("path");
 var db = require("../models")
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    db.Post.findAll({}).then(posts =>{
-      console.log("Posts: " , posts)
-      res.render("index", {blogs: posts})
+  app.get("/", function (req, res) {
+    db.Post.findAll({}).then(posts => {
+      console.log("Posts: ", posts)
+      res.render("index", { blogs: posts })
     })
   });
 
+  app.get("/create", function (req, res) {
+    res.render("create")
+  });
 
-  // // blog route loads blog.html
-  // app.get("/blog", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/blog.html"));
-  // });
+
+
 
 };
